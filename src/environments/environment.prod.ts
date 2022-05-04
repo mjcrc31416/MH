c08746db83c5760998855f0296a6a50ca9fdc2ea
Ruben <65301085+ruben868@users.noreply.github.com>
2022-05-04 17:16:22 -0500

    modificacion enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   //APIEndpoint: 'http://localhost:4390/api'
   //APIEndpoint: 'http://localhost:4390/api'
-  APIEndpoint: 'https://iphmov.azurewebsites.net/api'
-
+  //APIEndpoint: 'https://iphmov.azurewebsites.net/api'
+  APIEndpoint: 'https://cni05.sesnsp.net/iphmov/api'
 
   // test
 };
